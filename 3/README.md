e1fc96aba2ea38ee74aedcf2d0cfc92d03cdee7c
Hans-J. Schmid <knock@myopendoor.de>
2026-06-07 07:00:50 +0200

    gitignore

---
+++ 3/README.md
@@ -0,0 +1,2 @@
+- On the point of cloning a moved item in a closure:
+[Share Arc between Closures](https://stackoverflow.com/questions/53045522/share-arc-between-closures/53045864#53045864)
--- 3/hello-postgresql/src/main.rs
+++ 3/hello-postgresql/src/main.rs
@@ -1,7 +1,4 @@
-use actix_web::{
-    error::ErrorInternalServerError, web, App, Error,
-    HttpServer,
-};
+use actix_web::{error::ErrorInternalServerError, web, App, Error, HttpServer};
 use mobc::Pool;
 use mobc_postgres::PgConnectionManager;
 use openssl::ssl::{SslConnector, SslMethod};
@@ -9,46 +6,34 @@ use postgres_openssl::MakeTlsConnector;
 use std::{env, str::FromStr};
 use tokio_postgres::Config;
 
-type DatabasePool =
-    Pool<PgConnectionManager<MakeTlsConnector>>;
+type DatabasePool = Pool<PgConnectionManager<MakeTlsConnector>>;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let pool = create_pool()?;
-    let app = move || {
-        App::new()
-            .app_data(web::Data::new(pool.clone())) // why clone? `pool` is moved!
-            .service(
-                web::resource("/hello")
-                    .route(web::get().to(say_hello)),
-            )
-    };
-    Ok(HttpServer::new(app)
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await?)
+  let pool = create_pool()?;
+  let app = move || {
+    App::new()
+      .app_data(web::Data::new(pool.clone()))
+      .service(web::resource("/hello").route(web::get().to(say_hello)))
+  };
+  Ok(HttpServer::new(app).bind("127.0.0.1:8080")?.run().await?)
 }
 
 fn create_pool() -> anyhow::Result<DatabasePool> {
-    let database_url = env::var("DATABASE_URL")?;
-    let config = Config::from_str(&database_url)?;
-    let builder = SslConnector::builder(SslMethod::tls())?;
-    let tls = MakeTlsConnector::new(builder.build());
-    let manager = PgConnectionManager::new(config, tls);
-    let pool = Pool::builder().max_open(20).build(manager);
-    Ok(pool)
+  let database_url = env::var("DATABASE_URL")?;
+  let config = Config::from_str(&database_url)?;
+  let builder = SslConnector::builder(SslMethod::tls())?;
+  let tls = MakeTlsConnector::new(builder.build());
+  let manager = PgConnectionManager::new(config, tls);
+  let pool = Pool::builder().max_open(20).build(manager);
+  Ok(pool)
 }
 
-async fn say_hello(
-    pool: web::Data<DatabasePool>,
-) -> anyhow::Result<String, Error> {
-    let conn = pool
-        .get()
-        .await
-        .map_err(ErrorInternalServerError)?;
-    let result: tokio_postgres::Row = conn
-        .query_one("SELECT 'hello world'", &[])
-        .await
-        .map_err(ErrorInternalServerError)?;
-    Ok(result.get(0))
+async fn say_hello(pool: web::Data<DatabasePool>) -> anyhow::Result<String, Error> {
+  let conn = pool.get().await.map_err(ErrorInternalServerError)?;
+  let result: tokio_postgres::Row = conn
+    .query_one("SELECT 'hello world'", &[])
+    .await
+    .map_err(ErrorInternalServerError)?;
+  Ok(result.get(0))
 }
