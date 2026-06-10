040be3945fc06193c22097c9c3a9a3ecae004a22
Hans-J. Schmid <knock@myopendoor.de>
2026-06-10 23:36:36 +0200

    ch.5

--- 5/1-hello-sqlite/src/main.rs
+++ 5/1-hello-sqlite/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{error::ErrorInternalServerError, web, App, Error, HttpServer};
+use actix_web::{App, Error, HttpServer, error::ErrorInternalServerError, web};
+use anyhow::Result;
 use clap::Parser;
 use rusqlite::Connection;
 use std::env;
@@ -7,15 +8,16 @@ use std::sync::{Arc, Mutex};
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-  /// ID of the node.
   #[arg(short, long)]
-  id: u16,
+  node_id: u16,
 }
 
+type Conn = web::Data<Arc<Mutex<Connection>>>;
+
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> Result<()> {
   let args = Args::parse();
-  let port = 8080 + args.id;
+  let port = 8080 + args.node_id;
   let listen_addr = format!("127.0.0.1:{}", port);
   let database_url = env::var("DATABASE_URL")?;
   let conn = Arc::new(Mutex::new(Connection::open(database_url)?));
@@ -24,13 +26,15 @@ async fn main() -> anyhow::Result<()> {
       .app_data(web::Data::new(conn.clone()))
       .service(web::resource("/hello").route(web::get().to(say_hello)))
   };
+
   Ok(HttpServer::new(app).bind(listen_addr)?.run().await?)
 }
 
-async fn say_hello(conn: web::Data<Arc<Mutex<Connection>>>) -> anyhow::Result<String, Error> {
+async fn say_hello(conn: Conn) -> Result<String, Error> {
   let conn = conn.lock().unwrap();
   let result = conn
     .query_row("SELECT 'hello world'", [], |row| row.get(0))
     .map_err(ErrorInternalServerError)?;
+
   Ok(result)
 }
